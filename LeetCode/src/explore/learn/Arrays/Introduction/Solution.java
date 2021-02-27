7b8e2b35fc196d05f93e6ce263700e04d55b5881
이경민 <@host.docker.internal>
2021-02-27 23:58:30 +0900

    [LEETCODE] Arrays.INTRODUCTION.Find Numbers with Even Number of Digits

--- LeetCode/src/explore/learn/Arrays/Introduction/Solution.java
+++ LeetCode/src/explore/learn/Arrays/Introduction/Solution.java
@@ -43,10 +43,66 @@ public class Solution {
 		
 		return max;
     }
+	
+	/**
+	 * Find Numbers with Even Number of Digits
+	 * - 문제: Given an array nums of integers, return how many of them contain an even number of digits.
+	 * 
+	 * Example 1:
+
+		Input: nums = [12,345,2,6,7896]
+		Output: 2
+		Explanation: 
+		12 contains 2 digits (even number of digits). 
+		345 contains 3 digits (odd number of digits). 
+		2 contains 1 digit (odd number of digits). 
+		6 contains 1 digit (odd number of digits). 
+		7896 contains 4 digits (even number of digits). 
+		Therefore only 12 and 7896 contain an even number of digits.
+		
+	 * Example 2:
+		
+		Input: nums = [555,901,482,1771]
+		Output: 1 
+		Explanation: 
+		Only 1771 contains an even number of digits.
+		
+	 * Constraints:
+
+		1 <= nums.length <= 500
+		1 <= nums[i] <= 10^5
+	 * 
+	 * HINT
+	 *  - How to compute the number of digits of a number ?
+	 *  - Divide the number by 10 again and again to get the number of digits.
+	 */
+	public static int findNumbers(int[] nums) {
+		int divideCnt = 0;
+		int resultCnt = 0; 
+		
+		for (int num : nums) {
+			while (num > 9) {
+				num /= 10;
+				divideCnt++;
+			}
+			
+			if (divideCnt % 2 == 1) {
+				resultCnt++;
+			}
+			divideCnt = 0;
+		}
+		
+        return resultCnt;
+    }
 
 	public static void main(String[] args) {
-		int[] inputArr = {1,0,1,1,0,1};
-		System.out.println(findMaxConsecutiveOnes(inputArr));
+		// Max Consecutive Ones
+		// int[] inputArr = {1,0,1,1,0,1};
+		// System.out.println(findMaxConsecutiveOnes(inputArr));
+		
+		// Find Numbers with Even Number of Digits
+		int[] inputArr = {555,901,482,1771};
+		System.out.println(findNumbers(inputArr));
 	}
 
 }
