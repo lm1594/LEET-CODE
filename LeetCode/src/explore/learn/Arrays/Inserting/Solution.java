d4e118426d5462e0565f81bfbe49ebe8f39d3222
이경민 <@host.docker.internal>
2021-03-01 12:58:34 +0900

    [LEETCODE] Arrays.Inserting.Merge Sorted Array

--- LeetCode/src/explore/learn/Arrays/Inserting/Solution.java
+++ LeetCode/src/explore/learn/Arrays/Inserting/Solution.java
@@ -1,5 +1,7 @@
 package explore.learn.Arrays.Inserting;
 
+import java.util.Arrays;
+
 /**
  * LEET CODE
  *  - Explore.Learn.Arrays.Inserting Items Into Array
@@ -52,11 +54,56 @@ public class Solution {
         	System.out.print(num + " ");
     }
 	
+	/**
+	 * Merge Sorted Array
+	 *  - 문제: Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+		The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+	 *
+	 * Example 1:
+
+		Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+		Output: [1,2,2,3,5,6]
+		
+	 * Example 2:
+		
+		Input: nums1 = [1], m = 1, nums2 = [], n = 0
+		Output: [1]
+		 
+		
+	 * Constraints:
+		
+		nums1.length == m + n
+		nums2.length == n
+		0 <= m, n <= 200
+		1 <= m + n <= 200
+		-109 <= nums1[i], nums2[i] <= 109
+	 * 
+	 * HINT
+	 *  - You can easily solve this problem if you simply think about two elements at a time rather than two arrays. We know that each of the individual arrays is sorted. What we don't know is how they will intertwine. Can we take a local decision and arrive at an optimal solution?
+	 *  - If you simply consider one element each at a time from the two arrays and make a decision and proceed accordingly, you will arrive at the optimal solution.
+	 */
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j=0;
+		for(int i=m ; i<m+n ; i++) {
+        	nums1[i] = nums2[j++];
+        }
+        
+		Arrays.sort(nums1);
+    }
+	
 	
 	public static void main(String[] args) {
 		// Duplicate Zeros
-		int[] inputArr = {1,2,3};
-		duplicateZeros(inputArr);
+		// int[] inputArr = {1,2,3};
+		// duplicateZeros(inputArr);
+		
+		// Merge Sorted Array
+		int[] nums1 = {1,2,3,0,0,0};
+		int m = 3;
+		int[] nums2 = {2,5,6};
+		int n = 3;
+		merge(nums1, m, nums2, n);
 	}
 
 }
